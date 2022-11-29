736079a8ccd1e6229396cea2396a666bd0eb1534
Caarlos94 <carlos94_cr7@hotmail.com>
2022-11-28 20:54:42 -0600

    Modificación de PORT

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
